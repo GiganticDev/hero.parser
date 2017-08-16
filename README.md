4feefc849ccacd5c5eba58e5168840e97b198ae0
MEXdave1997 <david.mhernandez97@outlook.com>
2017-08-16 13:03:52 -0700

    complete docstring inclusion and README update

--- README.md
+++ README.md
@@ -2,18 +2,27 @@
 
 A Module to Parse Data from *Motiga's* **Gigantic**
 
+## Purpose:
+This community built API is made with the hopes of grabbing as much Hero data as possible. This will allow people to learn more about their favorite hero. 
 
 ## Installation:
 
-#### During Production:
+#### For Production:
 ```
 pip install .
 ```
 
-#### During development:
+#### For development:
 ```
 pip install -e .[development]
 ```
 
 Run `python -m gigantic.parser` in the 'RxGame' directory of your gigantic folder and it will print out json parsed output.
 You may use `python -m gigantic.parser > target_file.json` to output this to a file.
+
+
+## TODO: 
+
+* Successfully Parse references to ALL game heroes.
+* Change In Game Names with Official Character Name.
+* A Method to Store this information.
--- gigantic/parser/model.py
+++ gigantic/parser/model.py
@@ -14,27 +14,24 @@ class Resource(dict):  # Abstraction, no real sections in the ini
     """
     Grab Any Resource reference for each Hero.
     This includes references like
-    _Cooldown_Upgrade
+    _Cooldown_Upgrade.
     """
     resourceid = None  # puAdept4_Cooldown_Upgrade, Adept, etc etc
 
-    def __repr__(self):
-        return self.resourceid
-
 
 class UIResource(Resource):  # Abstraction, no real sections in the ini
-    """ Grab Resources used by the character (essentiall summons) """
+    """ Grab Resources used by the character (essentially summons). """
     uiresourceid = ''  # Adept, AdeptGhost, etc
 
 
 class Skill(UIResource):  # [ResourceID RxSkillProvider]
-    """ Grab All Available Skills for the Hero """
+    """ Grab All Available Skills for the Hero. """
     heroarchetypename = ''  # Adept, same as HeroArchetypeName in Archetype
     skillname = ''  # Skill1, Skill2, Skill3, or Skill3 typically
 
 
 class Upgrade(Resource):  # Abstraction, no real sections in the ini
-    """ Display Upgrades Paths, and information regarding each upgrade. """
+    """ Grab Upgrades Paths, and information regarding each upgrade. """
     heroname = ''  # Adept, same as HeroArchetypeName
     upgradetier = ''  # ESUT_Upgrade[1, 2], ESUT_Upgrade1_SubUpgrade[1, 2], ESUT_Upgrade2, ESUT_Upgrade2_SubUpgrade1, ESUT_None, etc
     minherolevel = 0  # 1 or 5?
@@ -42,32 +39,34 @@ class Upgrade(Resource):  # Abstraction, no real sections in the ini
 
 
 class SkillUpgrade(Upgrade):  # [ResourceID RxSkillUpgradeProvider]
+    """ Grab Skill Upgrade Information. """
     skillupgradecategory = ''  # EUC_Skill1Upgrade where Skill1 is the 'SkillName' of the skill
     skillindex = 0  # Not sure what it corresponds to, have seen anywhere from 11-34
 
 
 class PassiveUpgrade(Resource):  # [ResourceID RxPassiveUpgradeProvider]
+    """ Grab Passive Upgrade Information. """
     passiveupgradecategory = ''  # EUC_UnlockedDuringClash
     passiveiconidentifier = ''  # AttackFocus
     passiveindex = 0  # 4, 5, 6?
 
 
 class UpgradePath(Resource): # [ResourceID RxUpgradePathProvider]
+    """ Grab Upgrade Path Information. """
     setname = ''  # up_Adept_Default
     upgradetype = ''  # UPT_Skill1_U1
     groupindex = 0  # 0 - 15 in increments of 1?
 
-    def __repr__(self):
-        return str(self.setname)
-
 
 class SummonProvider(UIResource):  # [ResourceID RxSummonProvider]
+    """ Grab Summon Information. """
     providerpawnclasspath = ''  # "RxGameContent.RxPawn_AdeptAttacker"
     summonbehaviortreename = ''  # "BT_AdeptAttacker"
     creatorid = ''  # Adept (Maybe HeroArchetypeName?)
 
 
 class Archetype(UIResource):  # [HeroArchetypeName RxHeroProvider]
+    """ Grab Hero Archetype Information. """
     heroarchetypename = ''  # Adept_Pawn_Arch
     providerpawnclasspath = ''  # "RxGameContent.RxPawn_Adept"
     datasortpriority = 1700  # 1700
@@ -85,11 +84,9 @@ class Archetype(UIResource):  # [HeroArchetypeName RxHeroProvider]
     lowstaminatriggerpercent = 25.0  # 25.0f
     lowstaminarecoveredpercent = 30.0  # 30.0f
 
-    def __repr__(self):
-        return self.heroarchetypename
-
 
 class_section_map = {
+    """ Map the Gathered Information. """    
     'RxHeroProvider': Archetype,
     # 'RxSummonProvider': SummonProvider,
     'RxSkillProvider': Skill,
